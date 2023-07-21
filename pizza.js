ebc64e656466fe6838acde00b8174a01f19aa6fe
Mercyfulll <mercyful.buthelezi@gmail.com>
2023-07-21 17:12:15 +0200

    third commit

--- pizza.js
+++ pizza.js
@@ -11,10 +11,16 @@ document.addEventListener("alpine:init", () => {
                 const getCartUrl = `https://pizza-api.projectcodex.net/api/pizza-cart/${this.cartId}/get`
                 return axios.get(getCartUrl)
             },
+            addPizza(pizzaId){
+                return axios.post('https://pizza-api.projectcodex.net/api/pizza-cart/add',{
+                "cart_code": this.cartId,
+                "pizza_id" : pizzaId
+                })
+            },
             showCartData(){
                 this.getCart().then(result =>{
                     const cartData = result.data;
-                    this.cartPizzas = result.data.pizzas;
+                    this.cartPizzas = cartData.pizzas;
                     this.cartTotal = cartData.total.toFixed(2);
                 })
             },
@@ -23,10 +29,18 @@ document.addEventListener("alpine:init", () => {
                     .get('https://pizza-api.projectcodex.net/api/pizzas')
                     .then(result => {
                         console.log(result.data);
-                        this.pizzas = result.data.pizzas
+                        this.pizzas = result.data.pizzas;
                     })
                     this.showCartData()
                     
+            },
+            addPizzaToCart(pizzaId){
+                this.addPizza(pizzaId).then(()=>{
+                    this.showCartData()
+            })
+            },
+            removePizzaFromCart(pizzaId){
+
             }
         }
     })
